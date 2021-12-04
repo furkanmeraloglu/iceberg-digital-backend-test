ae695f5c112c2f51afab904ae0c9ee534d0ce892
Furkan Meraloğlu <furkanmeraloglu@gmail.com>
2021-12-05 00:54:54 +0300

    Contact model interface and repository modified

--- app/Repository/Interfaces/ContactRepositoryInterface.php
+++ app/Repository/Interfaces/ContactRepositoryInterface.php
@@ -2,7 +2,12 @@
 
 namespace App\Repository\Interfaces;
 
+use Illuminate\Database\Eloquent\Model;
+
 interface ContactRepositoryInterface
 {
-
+    public function getContact($id) : Model;
+    public function create(array $attributes) : Model;
+    public function update($id, array $attributes) : Model;
+    public function delete($id) : bool;
 }
--- app/Repository/Repositories/ContactRepository.php
+++ app/Repository/Repositories/ContactRepository.php
@@ -2,7 +2,31 @@
 
 namespace App\Repository\Repositories;
 
-class ContactRepository
+use App\Repository\Interfaces\ContactRepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Contact;
+
+class ContactRepository implements ContactRepositoryInterface
 {
 
+    public function getContact($id): Model
+    {
+        return Contact::findOrFail($id);
+    }
+
+    public function create(array $attributes): Model
+    {
+        return Contact::create($attributes);
+    }
+
+    public function update($id, array $attributes): Model
+    {
+        $contact = Contact::findOrFail($id);
+        return $contact->update($attributes);
+    }
+
+    public function delete($id): bool
+    {
+        return Contact::findOrFail($id)->delete();
+    }
 }
