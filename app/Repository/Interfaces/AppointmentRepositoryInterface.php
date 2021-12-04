e01318f829553061462be2eb6ecd9fd6f3f7002b
Furkan Meraloğlu <furkanmeraloglu@gmail.com>
2021-12-05 01:21:40 +0300

    Appointment interface and repository modified

--- app/Repository/Interfaces/AppointmentRepositoryInterface.php
+++ app/Repository/Interfaces/AppointmentRepositoryInterface.php
@@ -7,10 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 interface AppointmentRepositoryInterface
 {
-    public function getAll() : Collection;
-    public function getByQuery($query) : ?Collection;
+    public function getAll($filter = null) : Collection;
     public function create(array $attributes) : Model;
     public function update($id, array $attributes) : Model;
-    public function restore($id) : bool;
     public function delete($id) : bool;
 }
--- app/Repository/Repositories/AppointmentRepository.php
+++ app/Repository/Repositories/AppointmentRepository.php
@@ -2,33 +2,36 @@
 
 namespace App\Repository\Repositories;
 
-use App\Repository\AppointmentRepositoryInterface;
+
 use App\Models\Appointment;
+use App\Repository\Interfaces\AppointmentRepositoryInterface;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 class AppointmentRepository implements AppointmentRepositoryInterface
 {
-    /**
-     * @return Appointment[]|Collection
-     */
-    public function getAllAppointments()
+    public function getAll($filter = null): Collection
     {
-        return Appointment::all();
+        if ($filter === 'ASC')
+        {
+            return Appointment::orderBy('created_at', 'ASC')->get();
+        }
+        return Appointment::orderBy('created_at', 'DESC')->get();
     }
-    public function getAppointmentsByDate($appointmentId)
-    {
 
+    public function create(array $attributes): Model
+    {
+        return Appointment::create($attributes);
     }
-    public function createAppointment(array $appointmentDetails)
-    {
 
+    public function update($id, array $attributes): Model
+    {
+        $appointment = Appointment::findOrFail($id);
+        return $appointment->update($attributes);
     }
-    public function updateAppointment($appointmentId, array $appointmentDetails)
-    {
 
-    }
-    public function deleteAppointment($appointmentId)
+    public function delete($id): bool
     {
-
+        return Appointment::findOrFail($id)->delete();
     }
 }
