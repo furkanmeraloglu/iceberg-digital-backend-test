7f95b2fc9153c4279e66f37f3c160d81ae49183d
Furkan Meraloğlu <furkanmeraloglu@gmail.com>
2021-12-05 00:55:13 +0300

    StoreContactRequest modified

--- app/Http/Requests/StoreContactRequest.php
+++ app/Http/Requests/StoreContactRequest.php
@@ -2,29 +2,41 @@
 
 namespace App\Http\Requests;
 
+use ArondeParon\RequestSanitizer\Sanitizers\FilterVars;
+use ArondeParon\RequestSanitizer\Sanitizers\Lowercase;
+use ArondeParon\RequestSanitizer\Sanitizers\Trim;
+use ArondeParon\RequestSanitizer\Traits\SanitizesInputs;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
 
 class StoreContactRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
+    use SanitizesInputs;
+
+    protected $sanitizers = [
+        'name' => [FilterVars::class => ['filter' => FILTER_SANITIZE_STRING]],
+        'email' => [Lowercase::class, Trim::class, FilterVars::class => ['filter' => FILTER_SANITIZE_EMAIL]],
+        'phone' => [Trim::class, FilterVars::class => ['filter' => FILTER_SANITIZE_STRING]]
+    ];
+
+    public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
+    public function rules(): array
     {
         return [
-            //
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required|regex:/(01)[0-9]{9}+/|min:10'
         ];
+    }
+
+    protected function failedValidation(Validator $validator) : JsonResponse
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
     }
 }
