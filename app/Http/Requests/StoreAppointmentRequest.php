f0cbb668aa783035e3b1f8fde735b9bdd4967168
Furkan Meraloğlu <furkanmeraloglu@gmail.com>
2021-12-05 03:11:26 +0300

    StoreAppointmentRequest updated

--- app/Http/Requests/StoreAppointmentRequest.php
+++ app/Http/Requests/StoreAppointmentRequest.php
@@ -2,29 +2,40 @@
 
 namespace App\Http\Requests;
 
+use ArondeParon\RequestSanitizer\Sanitizers\FilterVars;
+use ArondeParon\RequestSanitizer\Sanitizers\Lowercase;
+use ArondeParon\RequestSanitizer\Sanitizers\Trim;
+use ArondeParon\RequestSanitizer\Traits\SanitizesInputs;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
 
 class StoreAppointmentRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
+    use SanitizesInputs;
+    protected $sanitizers = [
+        'postcode' => [Trim::class, Lowercase::class, FilterVars::class => ['filter' => FILTER_SANITIZE_STRING]],
+        'name' => [FilterVars::class => ['filter' => FILTER_SANITIZE_STRING]],
+        'email' => [Lowercase::class, Trim::class, FilterVars::class => ['filter' => FILTER_SANITIZE_EMAIL]],
+        'phone' => [Trim::class, FilterVars::class => ['filter' => FILTER_SANITIZE_STRING]]
+    ];
+    public function authorize(): bool
     {
-        return false;
+        return true;
     }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
+    public function rules(): array
     {
         return [
-            //
+            'postcode' => 'required|string|min:4',
+            'date' => 'required|date',
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required|regex:/(01)[0-9]{9}+/|min:10',
         ];
+    }
+    protected function failedValidation(Validator $validator) : JsonResponse
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
     }
 }
