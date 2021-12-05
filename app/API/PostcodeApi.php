8ed6c4c64ecc64c5967afc0104d0fa5fd1fd8722
Furkan Meraloğlu <furkanmeraloglu@gmail.com>
2021-12-05 17:41:28 +0300

    Postcode.io API HTTP connection built

--- app/API/PostcodeApi.php
+++ app/API/PostcodeApi.php
@@ -2,7 +2,27 @@
 
 namespace App\API;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Http;
+
 class PostcodeApi
 {
-
+    public static function getDestination($postcode) : JsonResponse
+    {
+        $response = Http::get("api.postcodes.io/postcodes/{$postcode}/validate");
+        if ($response->status() !== 200) {
+            return response()->json(['message' => 'The postcode is not valid!']);
+        }
+        $destination = Http::get("api.postcodes.io/postcodes/{$postcode}");
+        return response()->json([
+            'destination' => $destination,
+        ]);
+    }
+    public static function getOrigin($home_postcode) : JsonResponse
+    {
+        $origin = Http::get("api.postcodes.io/postcodes/{$home_postcode}");
+        return response()->json([
+            'origin' => $origin
+        ]);
+    }
 }
