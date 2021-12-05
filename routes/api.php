4ea175a08c8eb14f6f55ee3e159c8df29ade2a7d
Furkan Meraloğlu <furkanmeraloglu@gmail.com>
2021-12-05 05:39:12 +0300

    Routes updated

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AppointmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -15,14 +16,16 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-
-], function ($router) {
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::get('/user-profile', [AuthController::class, 'userProfile']);
+});
+Route::group(['middleware' => 'api', 'prefix' => 'appointments'], function($router){
+   Route::get('/', [AppointmentController::class, 'index']);
+   Route::post('/', [AppointmentController::class, 'store']);
+   Route::put('/{id}', [AppointmentController::class, 'update']);
+   Route::delete('/{id}', [AppointmentController::class, 'destroy']);
 });
