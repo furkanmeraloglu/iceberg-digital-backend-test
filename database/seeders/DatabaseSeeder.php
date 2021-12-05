e4f89fdccdaece8fcd79abbcb7ea66ccc9217fef
Furkan Meraloğlu <furkanmeraloglu@gmail.com>
2021-12-05 03:09:20 +0300

    New database seeder created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Appointment;
 use App\Models\Contact;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,14 +17,42 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
-        Appointment::factory(10)->create();
-        Contact::factory(10)->create();
-        foreach (Appointment::all() as $appointment)
-        {
-            $appointment->contacts()->attach(
-                Contact::inRandomOrder()->take(1)->pluck('id')
-            );
-        }
+        User::factory(5)->create();
+
+        $contact1 = new Contact();
+        $contact1->name = 'Büşra Yurt';
+        $contact1->email = 'billgates@mail.com';
+        $contact1->phone = +905555555555;
+        $contact1->save();
+        $appointment1 = new Appointment();
+        $appointment1->user_id = User::inRandomOrder()->first()->id;
+        $appointment1->contact_id = Contact::find(1)->id;
+        $appointment1->postcode = 'M320JG';
+        $appointment1->date = Carbon::now()->addWeekday(2);
+        $appointment1->save();
+
+        $contact2 = new Contact();
+        $contact2->name = 'Arda Yurt';
+        $contact2->email = 'jonathanreinink@mail.com';
+        $contact2->phone = +905555555554;
+        $contact2->save();
+        $appointment2 = new Appointment();
+        $appointment2->user_id = User::inRandomOrder()->first()->id;
+        $appointment2->contact_id = Contact::find(2)->id;
+        $appointment2->postcode = 'NE301DP';
+        $appointment2->date = Carbon::now()->addWeekday(4);
+        $appointment2->save();
+
+        $contact3 = new Contact();
+        $contact3->name = 'George Otwell';
+        $contact3->email = 'georgeotwell@mail.com';
+        $contact3->phone = +905555555553;
+        $contact3->save();
+        $appointment13 = new Appointment();
+        $appointment13->user_id = User::inRandomOrder()->first()->id;
+        $appointment13->contact_id = Contact::find(3)->id;
+        $appointment13->postcode = 'OX495NU';
+        $appointment13->date = Carbon::now()->addWeekday(6);
+        $appointment13->save();
     }
 }
